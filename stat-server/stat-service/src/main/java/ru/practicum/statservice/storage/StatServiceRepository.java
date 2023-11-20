0db441db9e855288286fa8ef8faeb121f6b79959
ViktorB <vitekb650@gmail.com>
2023-11-21 00:11:34 +0300

    fix: unique

--- stat-server/stat-service/src/main/java/ru/practicum/statservice/storage/StatServiceRepository.java
+++ stat-server/stat-service/src/main/java/ru/practicum/statservice/storage/StatServiceRepository.java
@@ -12,18 +12,18 @@ import java.util.List;
 @Repository
 public interface StatServiceRepository extends JpaRepository<EndpointHit, Integer> {
 
-    @Query("select new ru.practicum.statdto.dto.ViewStatsDto(eh.app, eh.uri, count(distinct eh.id)) " +
-            "from EndpointHit as eh " +
-            "where eh.timestamp > ?1 and eh.timestamp < ?2 AND eh.uri IN ?3 " +
-            "group by eh.app, eh.uri  " +
-            "order by count (distinct eh.ip) DESC")
-    List<ViewStatsDto> getStat(LocalDateTime start, LocalDateTime end, List<String> uris);
-
     @Query("select new ru.practicum.statdto.dto.ViewStatsDto(eh.app, eh.uri, count(eh.id)) " +
             "from EndpointHit as eh " +
             "where eh.timestamp > ?1 and eh.timestamp < ?2 AND eh.uri IN ?3 " +
             "group by eh.app, eh.uri  " +
             "order by count (eh.ip) DESC")
+    List<ViewStatsDto> getStat(LocalDateTime start, LocalDateTime end, List<String> uris);
+
+    @Query("select new ru.practicum.statdto.dto.ViewStatsDto(eh.app, eh.uri, count(distinct eh.id)) " +
+            "from EndpointHit as eh " +
+            "where eh.timestamp > ?1 and eh.timestamp < ?2 AND eh.uri IN ?3 " +
+            "group by eh.app, eh.uri  " +
+            "order by count (distinct eh.ip) DESC")
     List<ViewStatsDto> getStatUniqueIp(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("select new ru.practicum.statdto.dto.ViewStatsDto(e.app, e.uri, count (e)) " +
